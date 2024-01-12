206612a1c471cd4c7803de5f1e1f6e953920881d
MARGARITA <110416649+RCodeGirl@users.noreply.github.com>
2024-01-12 14:15:12 +0200

    Notice Application
    Create a new application web api
    Add connection to Database, Mapping

---
+++ Notes.Application/Interfaces/INotesDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Domain;
+using System.Threading.Tasks;
+
+namespace Notes.Application.Interfaces
+{
+    public interface INotesDbContext
+    {
+        DbSet<Note> Notes { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
